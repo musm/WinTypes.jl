8cc82095790da56c073d2c186c91229004bbaa0a
Mustafa Mohamad <mus-m@outlook.com>
2020-07-29 02:03:37 -0400

    Add more aliases and organize list

--- docs/src/index.md
+++ docs/src/index.md
@@ -63,95 +63,121 @@ directly translatable without manually looking up the myriad Windows data types.
 
 ## Alias List
 
-The following aliases are defined:
+The aliases defined in this package are provided below. Refer to
+[https://docs.microsoft.com/en-us/windows/win32/winprog/windows-data-types](https://docs.microsoft.com/en-us/windows/win32/winprog/windows-data-types)
+for detailed documentation on each constant.
 ```
-FALSE = Cint(0)
-TRUE = Cint(1)
+FALSE     = Cint(0)
+TRUE      = Cint(1)
 
-BOOL = Cint
-BOOLEAN = BYTE
-BYTE = Cuchar
-CCHAR = Cchar
-CHAR = Cchar
-COLORREF = DWORD
-DWORD = Culong
+BYTE      = Cuchar
+WORD      = Cushort
+DWORD     = Culong
 DWORDLONG = UInt64
-DWORD32 = UInt32
-DWORD64 = UInt64
-FLOAT = Cfloat
-HACCEL = HANDLE
+DWORD32   = UInt32
+DWORD64   = UInt64
+
+BOOL      = Cint
+BOOLEAN   = BYTE
+
+CHAR      = Cchar
+CCHAR     = Cchar
+
+FLOAT     = Cfloat
+
+LONG      = Culong
+ULONG     = Culong
+LONG32    = Cint
+LONG64    = Int64
+LONG_PTR  = Int
+
+SHORT     = Cshort
+USHORT    = Cushort
+
+ATOM      = WORD
+LANGID    = WORD
+
+COLORREF  = DWORD
+LGRPID    = DWORD
+LCTYPE    = DWORD
+
+LCID      = DWORD
+
+INT       = Int32
+INT8      = Int8
+INT16     = Int16
+INT32     = Int32
+INT64     = Int64
+INT_PTR   = Int
+UINT_PTR  = UInt
+
+WPARAM    = UINT_PTR
+LPARAM    = LONG_PTR
+
 HANDLE = Ptr{Cvoid}
-HBITMAP = HANDLE
-HBRUSH = HANDLE
-HCOLORSPACE = HANDLE
-HCONV = HANDLE
-HCONVLIST = HANDLE
-HCURSOR = HICON
-HDC = HANDLE
-HDDEDATA = HANDLE
-HDESK = HANDLE
-HDROP = HANDLE
-HDWP = HANDLE
+
+HACCEL       = HANDLE
+HBITMAP      = HANDLE
+HBRUSH       = HANDLE
+HCOLORSPACE  = HANDLE
+HCONV        = HANDLE
+HCONVLIST    = HANDLE
+HDC          = HANDLE
+HDDEDATA     = HANDLE
+HDESK        = HANDLE
+HDROP        = HANDLE
+HDWP         = HANDLE
 HENHMETAFILE = HANDLE
-HFILE = Cint
-HFONT = HANDLE
-HGDIOBJ = HANDLE
-HGLOBAL = HANDLE
-HHOOK = HANDLE
-HICON = HANDLE
-HINSTANCE = HANDLE
-HKEY = HANDLE
-HKL = HANDLE
-HLOCAL = HANDLE
-HMENU = HANDLE
-HMETAFILE = HANDLE
-HMODULE = HANDLE
-HMONITOR = HANDLE
-HPALETTE = HANDLE
-HPEN = HANDLE
-HRESULT = Clong
-HRGN = HANDLE
-HRSRC = HANDLE
-HSZ = HANDLE
-HWINSTA = HANDLE
-HWND = HANDLE
-INT = Cint
+HFILE        = Cint
+HFONT        = HANDLE
+HGDIOBJ      = HANDLE
+HGLOBAL      = HANDLE
+HHOOK        = HANDLE
+HICON        = HANDLE
+HCURSOR      = HICON
+HINSTANCE    = HANDLE
+HKEY         = HANDLE
+HKL          = HANDLE
+HLOCAL       = HANDLE
+HMENU        = HANDLE
+HMETAFILE    = HANDLE
+HMODULE      = HANDLE
+HMONITOR     = HANDLE
+HPALETTE     = HANDLE
+HPEN         = HANDLE
+HRESULT      = Clong
+HRGN         = HANDLE
+HRSRC        = HANDLE
+HSZ          = HANDLE
+HWINSTA      = HANDLE
+HWND         = HANDLE
+
 PHANDLE = Ptr{HANDLE}
-PVOID = Ptr{Cvoid}
-LPVOID = Ptr{Cvoid}
-SHORT = Cshort
-UCHAR = Cuchar
-USHORT = Cushort
-VOID = Cvoid
-WCHAR = Cwchar_t
-WORD = Cushort
-PWCHAR = Ptr{WCHAR}
-PWORD = Ptr{WORD}
-LPWORD = Ptr{WORD}
-PDWORD = Ptr{DWORD}
+
+UCHAR   = Cuchar
+WCHAR   = Cwchar_t
+PWCHAR  = Ptr{WCHAR}
+
+PWORD   = Ptr{WORD}
+LPWORD  = Ptr{WORD}
+PDWORD  = Ptr{DWORD}
 LPDWORD = Ptr{DWORD}
 
-PSTR = Ptr{CHAR} # char*
-LPSTR = Ptr{CHAR} # char*
+VOID    = Cvoid
+PVOID   = Ptr{Cvoid}
+LPVOID  = Ptr{Cvoid}
+LPCVOID = Ptr{Cvoid}
 
-PCSTR  = Ptr{CHAR} # char*
-LPCSTR = Ptr{CHAR} # char*
+LPINT   = Ptr{Cint}
+PINT    = Ptr{Cint}
 
-PWSTR = Ptr{WCHAR} # wchar_t*
-LPWSTR = Ptr{WCHAR} # wchar_t*
+PSTR    = Ptr{CHAR}
+PCSTR   = Ptr{CHAR}
+LPSTR   = Ptr{CHAR}
+LPCSTR  = Ptr{CHAR}
 
-PCWSTR = Ptr{WCHAR} # wchar_t*
-LPCWSTR = Ptr{WCHAR} # const wchar_t*
-```
-
-
-# Detailed Definitions
-
-Below we include detailed documentation for a few of the aliases defined in this package.
-We refer to
-[https://docs.microsoft.com/en-us/windows/win32/winprog/windows-data-types](https://docs.microsoft.com/en-us/windows/win32/winprog/windows-data-types)
-for those not included below.
-
-```@autodocs
-Modules = [WinTypes]
+PWSTR   = Ptr{WCHAR}
+PCWSTR  = Ptr{WCHAR}
+LPWSTR  = Ptr{WCHAR}
+LPCWSTR = Ptr{WCHAR}
 ```
--- src/WinTypes.jl
+++ src/WinTypes.jl
@@ -4,331 +4,124 @@ module WinTypes
 # https://docs.microsoft.com/en-us/windows/win32/winprog/windows-data-types
 # https://docs.microsoft.com/en-us/windows/win32/learnwin32/windows-coding-conventions?redirectedfrom=MSDN
 
-const FALSE = Cint(0)
-const TRUE = Cint(1)
+const FALSE     = Cint(0)
+const TRUE      = Cint(1)
 
-const BOOL = Cint
-# const BOOLEAN = BYTE
-const BYTE = Cuchar
-const BOOLEAN = BYTE
-const CCHAR = Cchar
-const CHAR = Cchar
-# const COLORREF = DWORD
-const DWORD = Culong
-const COLORREF = DWORD
+const BYTE      = Cuchar
+const WORD      = Cushort
+const DWORD     = Culong
 const DWORDLONG = UInt64
-const DWORD32 = UInt32
-const DWORD64 = UInt64
-const FLOAT = Cfloat
-# const HACCEL = HANDLE
+const DWORD32   = UInt32
+const DWORD64   = UInt64
+
+const BOOL      = Cint
+const BOOLEAN   = BYTE
+
+const CHAR      = Cchar
+const CCHAR     = Cchar
+
+const FLOAT     = Cfloat
+
+const LONG      = Culong
+const ULONG     = Culong
+const LONG32    = Cint
+const LONG64    = Int64
+const LONG_PTR  = Int
+
+const SHORT     = Cshort
+const USHORT    = Cushort
+
+const ATOM      = WORD
+const LANGID    = WORD
+
+const COLORREF  = DWORD
+const LGRPID    = DWORD
+const LCTYPE    = DWORD
+
+const LCID      = DWORD
+
+const INT       = Int32
+const INT8      = Int8
+const INT16     = Int16
+const INT32     = Int32
+const INT64     = Int64
+const INT_PTR   = Int
+const UINT_PTR  = UInt
+
+const WPARAM    = UINT_PTR
+const LPARAM    = LONG_PTR
+
+#############################################
+
 const HANDLE = Ptr{Cvoid}
-const HACCEL = HANDLE
-const HBITMAP = HANDLE
-const HBRUSH = HANDLE
-const HCOLORSPACE = HANDLE
-const HCONV = HANDLE
-const HCONVLIST = HANDLE
-# const HCURSOR = HICON
-const HDC = HANDLE
-const HDDEDATA = HANDLE
-const HDESK = HANDLE
-const HDROP = HANDLE
-const HDWP = HANDLE
+
+const HACCEL       = HANDLE
+const HBITMAP      = HANDLE
+const HBRUSH       = HANDLE
+const HCOLORSPACE  = HANDLE
+const HCONV        = HANDLE
+const HCONVLIST    = HANDLE
+const HDC          = HANDLE
+const HDDEDATA     = HANDLE
+const HDESK        = HANDLE
+const HDROP        = HANDLE
+const HDWP         = HANDLE
 const HENHMETAFILE = HANDLE
-const HFILE = Cint
-const HFONT = HANDLE
-const HGDIOBJ = HANDLE
-const HGLOBAL = HANDLE
-const HHOOK = HANDLE
-const HICON = HANDLE
-const HCURSOR = HICON
-const HINSTANCE = HANDLE
-const HKEY = HANDLE
-const HKL = HANDLE
-const HLOCAL = HANDLE
-const HMENU = HANDLE
-const HMETAFILE = HANDLE
-const HMODULE = HANDLE
-const HMONITOR = HANDLE
-const HPALETTE = HANDLE
-const HPEN = HANDLE
-const HRESULT = Clong
-const HRGN = HANDLE
-const HRSRC = HANDLE
-const HSZ = HANDLE
-const HWINSTA = HANDLE
-const HWND = HANDLE
-const INT = Cint
+const HFILE        = Cint
+const HFONT        = HANDLE
+const HGDIOBJ      = HANDLE
+const HGLOBAL      = HANDLE
+const HHOOK        = HANDLE
+const HICON        = HANDLE
+const HCURSOR      = HICON
+const HINSTANCE    = HANDLE
+const HKEY         = HANDLE
+const HKL          = HANDLE
+const HLOCAL       = HANDLE
+const HMENU        = HANDLE
+const HMETAFILE    = HANDLE
+const HMODULE      = HANDLE
+const HMONITOR     = HANDLE
+const HPALETTE     = HANDLE
+const HPEN         = HANDLE
+const HRESULT      = Clong
+const HRGN         = HANDLE
+const HRSRC        = HANDLE
+const HSZ          = HANDLE
+const HWINSTA      = HANDLE
+const HWND         = HANDLE
+
+#############################################
+
 const PHANDLE = Ptr{HANDLE}
-const PVOID = Ptr{Cvoid}
-const LPVOID = Ptr{Cvoid}
-const SHORT = Cshort
-const UCHAR = Cuchar
-const USHORT = Cushort
-const VOID = Cvoid
-const WCHAR = Cwchar_t
-const WORD = Cushort
-const PWCHAR = Ptr{WCHAR}
-const PWORD = Ptr{WORD}
-const LPWORD = Ptr{WORD}
-const PDWORD = Ptr{DWORD}
+
+const UCHAR   = Cuchar
+const WCHAR   = Cwchar_t
+const PWCHAR  = Ptr{WCHAR}
+
+const PWORD   = Ptr{WORD}
+const LPWORD  = Ptr{WORD}
+const PDWORD  = Ptr{DWORD}
 const LPDWORD = Ptr{DWORD}
 
-const PSTR = Ptr{CHAR} # char*
-const LPSTR = Ptr{CHAR} # char*
-
-const PCSTR  = Ptr{CHAR} # const char*
-const LPCSTR = Ptr{CHAR} # const char*
-
-const PWSTR = Ptr{WCHAR} # wchar_t*
-const LPWSTR = Ptr{WCHAR} # wchar_t*
-
-const PCWSTR = Ptr{WCHAR} # const wchar_t*
-const LPCWSTR = Ptr{WCHAR} # const wchar_t*
-
-
-"""
-    const BOOL = Cint
-
-A Boolean variable (should be TRUE or FALSE).
-
-This type is declared in `WinDef.h` as follows:
-
-`typedef int BOOL;`
-
-BOOL is a typedef for an integer value that is used in a Boolean context. The header file
-`WinDef.h` also defines two values for use with BOOL.
-
-```
-#define FALSE    0
-#define TRUE     1
-```
-
-See also: [`TRUE`](@ref) and [`FALSE`](@ref)
-
-"""
-BOOL
-
-
-"""
-    const FALSE = Cint(0)
-
-A Boolean variable denoting FALSE.
-"""
-FALSE
-
-
-"""
-    const TRUE = Cint(1)
-
-A Boolean variable denoting FALSE. Should only be used in specialized contexts since most functions
-that reutnr a `BOOL` type can return any non-zero value to indicate Boolean truth.
-"""
-TRUE
-
-"""
-    const BYTE = Cuchar
-
-A byte (8 bits).
-
-This type is declared in `WinDef.h` as follows:
-
-`typedef unsigned char BYTE;`
-"""
-BYTE
-
-"""
-    const BOOLEAN = BYTE
-
-A Boolean variable (should be TRUE or FALSE).
-
-This type is declared in `WinNT.h` as follows:
-
-`typedef BYTE BOOLEAN;`
-"""
-BOOLEAN
-
-"""
-    const CCHAR = Cchar
-
-An 8-bit Windows (ANSI) character.
-
-This type is declared in `WinNT.h` as follows:
-
-`typedef char CCHAR;`
-"""
-CCHAR
-
-"""
-    const CCHAR = Cchar
-
-An 8-bit Windows (ANSI) character.
-
-This type is declared in `WinNT.h` as follows:
-
-`typedef char CHAR;`
-"""
-CHAR
-
-"""
-    const DWORD = Culong
-
-A 32-bit unsigned integer.
-
-This type is declared in `IntSafe.h` as follows:
-
-`typedef unsigned long DWORD;`
-"""
-DWORD
-
-"""
-    const DWORDLONG = UInt64
-
-A 64-bit unsigned integer.
-
-This type is declared in `IntSafe.h` as follows:
-
-`typedef unsigned __int64 DWORDLONG;`
-"""
-DWORDLONG
-
-"""
-    const DWORD32 = UInt32
-
-A 32-bit unsigned integer.
-
-This type is declared in `BaseTsd.h` as follows:
-
-`typedef unsigned int DWORD32;`
-"""
-DWORD32
-
-"""
-    const DWORD64 = UInt64
-
-A 64-bit unsigned integer.
-
-This type is declared in `IntSafe.h` as follows:
-
-`typedef unsigned __int64 DWORD64;`
-"""
-DWORD64
-
-"""
-    const FLOAT = Cfloat
-
-A floating-point variable.
-
-This type is declared in `WinDef.h` as follows:
-
-`typedef float FLOAT;`
-"""
-FLOAT
-
-"""
-    const HANDLE = Ptr{Cvoid}
-
-A handle to an object.
-
-This type is declared in `WinNT.h` as follows:
-
-`typedef PVOID HANDLE;`
-"""
-HANDLE
-
-
-
-
-"""
-    const HKEY = HANDLE
-
-A handle to a registry key.
-
-This type is declared in `WinDef.h` as follows:
-
-`typedef HANDLE HKEY;`
-"""
-HKEY
-
-"""
-    cosnt PVOID = Ptr{Cvoid}
-
-A pointer to any type.
-
-This type is declared in `WinNT.h` as follows:
-
-`typedef void *PVOID;`
-"""
-PVOID
-
-"""
-    const SHORT = Cshort
-
-A 16-bit integer.
-
-This type is declared in `WinNT.h` as follows:
-
-`typedef short SHORT;`
-"""
-SHORT
-
-
-"""
-    const UCHAR = Cuchar
-
-An unsigned CHAR.
-
-This type is declared in `WinDef.h` as follows:
-
-typedef unsigned char UCHAR
-"""
-UCHAR
-
-
-"""
-    const USHORT = Cushort
-
-An unsigned SHORT.
-
-This type is declared in `WinDef.h` as follows:
-
-`typedef unsigned short USHORT;`
-"""
-USHORT
-
-"""
-    const VOID = Cvoid
-
-
-Any type.
-
-This type is declared in `WinNT.h` as follows:
-
-#define VOID void
-"""
-VOID
-
-
-"""
-    const WCHAR = Cwchar_t
-
-A 16-bit Unicode character.
-
-This type is declared in `WinNT.h` as follows:
-
-`typedef wchar_t WCHAR;`
-"""
-WCHAR
-
-"""
-    const WORD = Cushort
-
-A 16-bit unsigned integer.
-
-This type is declared in `WinDef.h` as follows:
-
-`typedef unsigned short WORD;`
-"""
-WORD
+const VOID    = Cvoid
+const PVOID   = Ptr{Cvoid}
+const LPVOID  = Ptr{Cvoid}
+const LPCVOID = Ptr{Cvoid}
+
+const LPINT   = Ptr{Cint}
+const PINT    = Ptr{Cint}
+
+#############################################
+
+const PSTR    = Ptr{CHAR}
+const PCSTR   = Ptr{CHAR}
+const LPSTR   = Ptr{CHAR}
+const LPCSTR  = Ptr{CHAR}
+
+const PWSTR   = Ptr{WCHAR}
+const PCWSTR  = Ptr{WCHAR}
+const LPWSTR  = Ptr{WCHAR}
+const LPCWSTR = Ptr{WCHAR}
 
 end
